d98b23b0ae9f8cbeb2647e574f579973674d7a19
YUNSUNGWOONG <145294961+JackSmith-c@users.noreply.github.com>
2024-02-23 04:17:01 +0900

    d

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,247 @@
+var store = [{
+        "title": "[MyLab]로컬에서 지킬서버 구동하기",
+        "excerpt":"지킬서버 구동하기 bundle exec jekyll serve 그 이외에도 다양한 명령어들이 있다.(링크) 지킬서버 구동시 발생하는 오류에 대해서… 24년1월1일, 지킬서버 구동시 이미지 폴더를 찾지 못하는 오류가 지속적으로 발생했다. 내 생각에 파일을 Typora에 가져와서 이 경로에 대해 http://localhost:4000구문을 이용해 동적파싱형식으로 설정을 변경하는 과정에서 꼬인 것 같다. 좀 풀어서 말하자면, 이미지를 가져오긴 했는데, 아직...","categories": ["2023-12"],
+        "tags": ["mylab"],
+        "url": "/2023-12/myjekyll/",
+        "teaser": null
+      },{
+        "title": "[MyLab]주피터 활용해보기",
+        "excerpt":"1. Header &amp; Sub-Header ※Syntax # This is H1 Header ## This is H2 Header ### This is H3 Header #### This is H4 Header ##### This is H5 Header ###### This is H6 Header ※Result This is H1 Header This is H2 Header This is H3 Header This is...","categories": ["2023-12"],
+        "tags": ["mylab"],
+        "url": "/2023-12/myjupyterapplication/",
+        "teaser": null
+      },{
+        "title": "[MyLab]Latex와 MathJax",
+        "excerpt":"1. Header &amp; Sub-Header ※Syntax # This is H1 Header ## This is H2 Header ### This is H3 Header #### This is H4 Header ##### This is H5 Header ###### This is H6 Header ※Result This is H1 Header This is H2 Header This is H3 Header This is...","categories": ["2023-12"],
+        "tags": ["mylab"],
+        "url": "/2023-12/mylatex/",
+        "teaser": null
+      },{
+        "title": "[MyLab]마크다운 문법활용",
+        "excerpt":"1. Header &amp; Sub-Header ※Syntax # This is H1 Header ## This is H2 Header ### This is H3 Header #### This is H4 Header ##### This is H5 Header ###### This is H6 Header ※Result This is H1 Header This is H2 Header This is H3 Header This is...","categories": ["2023-12"],
+        "tags": ["mylab"],
+        "url": "/2023-12/mysyntax/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2023-12-28)",
+        "excerpt":"이 코딩일지 블로그를 시작한 것에 대하여.. 기존에 네이버 블로그(yhon90)(링크)에다가 공부일지 블로그를 적으면서 코드도 같이 넣었는데 한가지 고질적인 문제라고 한다면, 공부일기에 일상얘기도 내가 넣어버리니까 얘기흐름이 꼬이는 것 같다는 생각이 들었다. 그래서 어차피 코딩 관련한 일기를 쓸때는 거의 무조건 랩탑을 들고 와서 진행중일테니까 일반 일기와 코딩일기로 일기를 이원화시키기로 했다. 코딩일기에서는 년도와 월별로...","categories": ["2023-12"],
+        "tags": ["C"],
+        "url": "/2023-12/codinglog(1)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2023-12-29)",
+        "excerpt":"double과 float, 그리고 %f와 %lf float로 선언할때는 %f를, double로 선언할 때는 %lf를 사용해야 한다. ※Code #define _CRT_SECURE_NO_WARNINGS #include &lt;stdio.h&gt; #include &lt;math.h&gt; int main() { double base, exponent, result; //사용자로부터 밑(base)과 지수(exponent) 입력받기 printf(\"밑(base)를 입력하세요: \"); scanf(\"%.1f\", &amp;base); printf(\"지수(exponent)를 입력하세요: \"); scanf(\"%.1f\", &amp;exponent); //pow() 함수를 사용하여 거듭제곱 계산 result = pow(base,...","categories": ["2023-12"],
+        "tags": ["C"],
+        "url": "/2023-12/codinglog(2)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2023-12-30)",
+        "excerpt":"[CSS]background와 background-color의 차이 현재 내가 제작중인 블로그에서 옆에 사이드바[메뉴]를 제작중이다. 근데 계속 꼬리표 모양이 tiktok에 대해서는 깨진다. 이거 이슈를 해결하는 과정에서 스타일을 함수로 처리하는 것도 보았다. 예전에는 이런게 없었는데ㄷㄷ HTML5까지 올라오면서 이것도 OOP를 조금씩 섞는게 아닌가 하는 생각을 잠깐 해보았다.(틀렸으면 말구~~) [HTML]code insert 위 이미지는 해당링크에서 참고한 코드이다. 이 코드를...","categories": ["2023-12"],
+        "tags": ["HTML","CSS","C"],
+        "url": "/2023-12/codinglog(3)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2023-12-31)",
+        "excerpt":"아직 블로그 제작중이다. 60%정도는 만들었다고 생각한다. (EP18. 이미지 추가시 오류 해결 (A/S 영상))[https://www.youtube.com/watch?v=ndJ5B-DyBnA] 이 영상을 보면서 카테고리를 생성하면서 잘못 설계된 상대경로를 절대경로로 재정립시켜주는 작업을 했다. 그리고 아래 처럼 중괄호를 두번쓰면 동적파싱이 된다는 것을 알게 되었다. site라는 객체 안에 &lt;pre&gt;url, title, description, logo&lt;/pre&gt;등 여러 프로퍼티들이 있다고 한다. http://localhost:4000 CodingLog(by JackSmith-c) 기억보다...","categories": ["2023-12"],
+        "tags": ["C","HTML"],
+        "url": "/2023-12/codinglog(4)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-01)",
+        "excerpt":"이제 2024년이다! 올해는 무조건 실적을 많이 낼 것이다. 올해만을 준비했다는 마음가짐으로 한해를 가치있게 살고 싶다 [백준]1330번 두 수 비교하기 위 문제의 정답은 어렵지 않다. #define _CRT_SECURE_NO_WARNINGS #include &lt;stdio.h&gt; int main(void) { long long a, b; scanf(\"%lld %lld\", &amp;a, &amp;b); if (-10000 &lt; a &amp;&amp; a &lt; 10000) { if (-10000...","categories": ["2024-01"],
+        "tags": ["C","HTML"],
+        "url": "/2024-01/codinglog(5)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-02)",
+        "excerpt":"[백준]15552번 빠른 A+B C++을 사용하고 있고, cin/cout을 사용하고자 한다면, cin.tie(NULL)과 sync_with_stdio(false)를 둘 다 적용해주고, endl 대신 개행문자(\\n)를 쓰자. 단, 이렇게 하면 더 이상 scanf/printf/puts/getchar/putchar 등 C의 입출력방식을 사용하면 안된다. Python을 사용하고 있다면, input대신 sys.stdin.readline을 사용할 수 있다. 단, 이때는 맨 끝의 개행문자까지 같이 입력받기 때문에 문자열을 저장하고 싶을 경우 .rstrip()을...","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(6)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-03)",
+        "excerpt":"PCA 알고리즘 순서도 import numpy as np from sklearn.datasets import load_iris iris = load_iris().data.copy() print('iris: \\n', iris[:10,:]) iris: [[5.1 3.5 1.4 0.2] [4.9 3. 1.4 0.2] [4.7 3.2 1.3 0.2] [4.6 3.1 1.5 0.2] [5. 3.6 1.4 0.2] [5.4 3.9 1.7 0.4] [4.6 3.4 1.4 0.3] [5. 3.4 1.5 0.2]...","categories": ["2024-01"],
+        "tags": ["python","C"],
+        "url": "/2024-01/codinglog(7)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2023-01-04)",
+        "excerpt":"내일은 중간고사이다. 어제랑 오늘 있을 실습과제를 해결해야 한다. 그리고 pdf에 있는 예제코드도 오늘 다 돌려보자. 그리고 1월2일에 있었던 실습문제 중, 버블정렬을 이용한 실습문제가 있었는데, 처음에 조금 헤맸었다. 한번 더 복습하자. [C]실습 8-2 이번 코드를 짤떄 한가지 흥미로웠던 것은 배수 처리이다. 무슨 말이나면, nxn정방행렬꼴로 값을 1부터 채워나가야 하는 형국인데, 언제 줄바꿈을...","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(8)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-05)",
+        "excerpt":"족보정리 [[필기유형][(다음코드의 출력문을 적어라)]]: 증가,감소 연산자 전/후 위치 차이 아는지 묻는 문제 (실기) (1)실수 입력 받아서 정수 부분, 실수 부분 따로 출력하기 (실기) (2)정수 a,b 받고 [a,b]에서 약수가 홀수개인 정수 개수 구하기 (실기) (3)크기 n과 n개의 수 입력받고 등차수열인지 등비수열인지 판단하기 (실기) (4)n과 n개의 수 입력 받고 평균과 표준편차 구하기...","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(9)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-06)",
+        "excerpt":"오늘부터 어도비 프리미어 공부를 시작했다. 이 부분에 대한 내용도 여기 코딩일지 사이트에 넣을까 하다가, 본래의 목적이 퇴색될 것 같아, 본래 네이버 블로그(yhon89)블로그에만 기록하기로 했다. 한가지 흥미로운 점은 프리미어에서는 proprj라는 확장자 파일을 사용한다는 것이었다. 반면에 비쥬얼스튜디오에서 Visual C++로 프로젝트를 만들면, vcproj/vcxproj라는 확장자 파일이 생성된다는 것이다. .VCXPROJ 파일이란? Visual CX[C++ Extension] Project의...","categories": ["2023-12"],
+        "tags": ["C"],
+        "url": "/2023-12/codinglog(10)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-07)",
+        "excerpt":"[백준] 10810번 공 넣기 #define _CRT_SECURE_NO_WARNINGS #include &lt;stdio.h&gt; typedef enum { EXIT, //0 HOLD_OFF //1 }STATE; STATE main(void) { int n, m; int basket[101] = { 0, }; int a, b, c; printf(\"input&gt; \\n\"); scanf(\"%d %d\", &amp;n, &amp;m); for (int i = 0; i &lt; m; i++) { scanf(\"%d %d...","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(11)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-08)",
+        "excerpt":"계절학기-후반부 스케줄 이수 과정 1_15.파일처리 - 텍스트파일(1) 2_11. 포인터 기초(1) 3_11.포인터 기초(2) 4_16. 동적할당(1) 5_14. 함수와 포인터 활용 6_12. 문자와 문자열 7_13. 구조체와 공용체 8_15. 파일처리 - 이진파일(2) 9_16. 동적할당(2) - 연결리스트, 전처리 시험관련 16일날 시험봄 필기 50분 실기 2시간 하 ㅅㅂ… 족보부터 돌려야 겠다. 파일처리(1) 이름과 성적 정보 내용으로...","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(12)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-09)",
+        "excerpt":"“#define NULL ((void *)0)”에 대해서(지난 복습) 이 코드는 C나 C++ 프로그래밍에서 NULL포인터를 정의하는 매크로입니다. 이 매크로는 보통 포인터가 가리키는 대상이 없음을 나타내는데 사용된다. 여기서, NULL은 포인터가 아무것도 가리키지 않음을 나타내기 위해 0으로 정의되어 있다고 한다. 일반적으로, NULL은 포인터가 유효한 주소를 가지고 있지 않다는 것을 나타내기 위해 사용됩니다. 코드에서 포인터 초기화...","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(13)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-10)",
+        "excerpt":"값에 의한 호출 vs 참조에 의한 호출 값에의한 호출: 복사본만 건드릴 수 있음.(원본은 못 건드림) 참조에 의한 호출: 원본의 주소를 넘겨받기 때문에 원본을 건드림. #pragma warning(disable : 4996) #include &lt;stdio.h&gt; #include &lt;stdlib.h&gt; #include &lt;ctype.h&gt; void increase(int origin, int increment) { origin += increment; } void incbyaddress(int* origin, int increment) {...","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(14)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-11)",
+        "excerpt":"문자와 문자열 출력 #define _CRT_SECURE_NO_WARNINGS #include &lt;stdio.h&gt; int main() { //문자 선언과 출력 char ch = 'A'; printf(\"%c %d\\n\", ch, ch); //문자열 선언 방법1 char java[] = { 'J', 'A', 'V', 'A', '\\0' }; printf(\"%s\\n\", java); //문자열 선언 방법2 char py[] = \"Python\"; printf(\"%s\\n\", py); //문자열 선언 방법3 char csharp[5]...","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(15)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-12)",
+        "excerpt":"01)구조체와 공용체 구조체 정의와 구조체 변수 선언 #define _CRT_SECURE_NO_WARNINGS #include &lt;stdio.h&gt; #include &lt;string.h&gt; //은행계좌정보를 위한 구조체 struct account { char name[12]; int actnum; double balance; }; int main() { //구조체 변수 선언 및 초기화 //1.mine이라는 이름을 구조체 정의하고 info 삽입, mine이 소유 struct account mine = {\"홍길동\", 1001, 300000}; //2.yours이라는...","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(16)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-13)",
+        "excerpt":"구조체 선언방식의 차이 struct account와 ‘typdef struct {…} account’는 구조체를 정의하는 두가지 방식이다. 여기까지만 차이가 존재하면 그냥 줄중에 보기 편한거 하나만 고정적으로 사용하면 되는데, 이러면 나중에 main안에서 선언하는 방식도 달라지기 떄문에, 제대로 짚고 넘어가는게 좋을 것 같아 정리한다. 1.struct account: struct account { char name[12]; int actnum; double balance; };...","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(17)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-14)",
+        "excerpt":"목요일 4번 문제 위 코드는 “f2.txt”파일에 있는 단어들을 동적할당된 이차원배열에서 받아가지고 qsort()를 써서 값을 오름차순으로 재배열 하는 문제이다. 이때 compare()함수를 구현해주어야 하는데, 이게 생각보다 성가시다ㅜㅠ. 그래서 이 부분에 대해서만 집중적으로 정리해보려고 한다. int compare(const void *a, const void *b) { return strcmp(*(const char **)a, *(const char **)b); } 코드리뷰: 여기서...","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(18)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-15)",
+        "excerpt":"01 파일기초 지난 금요일에 했음. 02 텍스트파일 입출력 지난 금요일에 했음. 03 이진파일 입출력 1)여러 줄에 걸쳐 학생 성적 구조체의 정보를 입력하여 파일을 생성하고 다시 파일에서 구조체 정보를 읽어 표준출력에 출력 #define _CRT_SECURE_NO_WARNINGS #include &lt;stdio.h&gt; #include &lt;stdlib.h&gt; #include &lt;string.h&gt; typedef struct { int number; // 번호 char name[40]; // 이름...","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(19)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-17)",
+        "excerpt":"제목1  제목2  제목3  제목4  제목5  제목6   이미지 삽입 문법(중앙정렬)  &lt;div style=\"text-align:center;\"&gt;     &lt;img src=\"{path_copy}\" alt=\"image-20231230154210231\" style=\"zoom:100%;\" /&gt; &lt;/div&gt;   중앙 정렬된 이미지 삽입 예시          이미지에 링크 삽입 문법  &lt;div style=\"text-align:center;\"&gt;     &lt;a href=\"https://www.cygwin.com/\" target=\"_blank\"&gt;         &lt;img src=\"/images/2024-01-26-codinglog(27)/image-20240127022122739.png\" alt=\"image-20231230154210231\" style=\"zoom:100%;\" /&gt;     &lt;/a&gt; &lt;/div&gt;   ","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(20)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-20)",
+        "excerpt":"리눅스 쉘 명령어-가상환경(env)나가고 들어오기 현재 라즈베리파이에서 cmd창을 열면 자동으로 (env)가 뜨는 것을 목격하였다. 아마 어제 donkeycar관련해서 여러가지 받으면서 (env)로 접속하는게 디폴트로 된 것 같다. 그래서 다시 원래대로 복구할까 하다가, 앞으로 이 sd카드를 동키카 전용으로 사용할거라서 그냥 두기로 했다. 만약 가상환경에서 나오고 싶다면 다음 명령어를 쳐주면 된다. &gt;&gt;deactivate 위 명령어를 입력하고...","categories": ["2024-01"],
+        "tags": ["shell"],
+        "url": "/2024-01/codinglog(21)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-21)",
+        "excerpt":"RaspberryPi 4B에서 Python 버전충돌없이 TensorFlow 설치하기 텐서플로우 2.5 이상은 python3.9부터 지원을 한다. 하지만 내가 갖고 있는 라즈베리파이에는 텐서플로우 2.4버전까지만 설치하기 쉽게 도와주는 whl파일이 있었다. 그런데 텐서플로우 2.4버전까지는 파이썬 3.7까지만 지원이 되었다. 그런데!! 파이썬은 보통 라즈비안 OS를 설치시 기본 설치되어서 나오기 때문에 OS버전을 제대로 먼저 확인을 하고 설치를 해야 한다. 파이썬...","categories": ["2024-01"],
+        "tags": ["shell"],
+        "url": "/2024-01/codinglog(22)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-22)",
+        "excerpt":"pyenv를 사용해야 한다고 했었다. 이거를 정리하고 가자. 라즈베리파이에서 콘솔창 관련 단축키 만약 gui창에서 콘솔창으로 넘어가야 한다면? ctrl + alt + F1 을 눌러주고, 만약 콘솔창에서 gui창으로 넘어가야 한다면? ctrl + alt + F7 을 눌러주면 된다. 그리고 gui창 안에서 콘솔창을 작게 열고 싶다면? ctrl + alt + T 를 눌러주면...","categories": ["2024-01"],
+        "tags": ["shell"],
+        "url": "/2024-01/codinglog(23)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-23)",
+        "excerpt":"라즈베리파이 이미지 설정시 주의할 점과 ssh키 만들기, 행렬미분 공부하기 라즈베리파이 이미지 초기설정하면서 느낀것, 아이디에는 문자가 들어가게 하자! [warning-issue] No session for pid XXX 처음에 이미지를 구울때 계속 No session for pid XXX라는 경고창이 뜨는 문제가 발생했었다. 모든 버전으로 다 설치를 해봐도 계속 문제가 생겼다. 이 문제를 해결하는데 3~4일은 사용한 것...","categories": ["2024-01"],
+        "tags": ["shell"],
+        "url": "/2024-01/codinglog(24)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-24)",
+        "excerpt":"데이터에 입각한 고전신경망과 현대신경망의 차이 9.1.1 전통적인 신경망 이번 장에서 전통적인 신경망이나 기타 고전적 기계학습 모형(지지벡터 기계, 무작위 숲 등)을 다룰 떄는 데이터를 2차원 배열로 표현하기로 합니다. 한 예로 제6장에서 소개한 붓꽃 데이터 집합의 각 특징 벡터는 특징 네개로 구성된다. 그런 특징 벡터들의 집합을 다음처럼 NumPy 행렬로 표현할 수 있다....","categories": ["2024-01"],
+        "tags": ["python","shell"],
+        "url": "/2024-01/codinglog(25)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-25)",
+        "excerpt":"동키카를 까는 험난한 여정 와이파이 연결상태에서 인터넷이 안될때 다음과 같이 Configuration에서 Localisation에 들어간다.. 그리고 “Set WLAN Country”선택! 보통 여기서 UK(Britian)으로 되어 있는 경우가 많은데, 그래서 안된거다.(근데 신기한게 와이파이로 원격은 된다. 이뭔????ㄷㄷ???) 쨋든 위와 같이 KR Korea(South)로 변경해준다. 근데 이렇게 해도 문제가 해결되지 않았다. (그래서 진짜 집에 갈까 진지하게 고민했다ㅅㅂ…) 결국...","categories": ["2024-01"],
+        "tags": ["shell"],
+        "url": "/2024-01/codinglog(26)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-26)",
+        "excerpt":"Cygwin 설치해보기 Cygwin을 설치해보았다. 오늘이나 내일중으로 나는 레노버 랩탑을 포맷할 예정이다.(여기까지는 이미 확정된 내용이다) 그래서 그 이전에 한번깔아보고 싶은 것들, 나중에 어차피 지울꺼라 상관없는 것들을 한번 깔아보려고 한다. (한번 깔아보면 그 경험이 내제되어 나중에 깔때는 좀 거리낌없이 깔수있게 되기 때문이다.) 그래서 저번에 msys2관련 문서를 보던중에 알게된 cygwin에 대해 알아보고 설치를...","categories": ["2024-01"],
+        "tags": ["shell"],
+        "url": "/2024-01/codinglog(27)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-27)",
+        "excerpt":"Keil uVision 설치 아직 랩탑을 포맷하지 않았다. 이번에는 3학년 2학기때 마이크로프로세서 설계실험과목에서 사용했던 툴인 Keil uVision을 설치해보기로 했다. 여기 (링크)를 보고 따라서 해보았다. MDK-ARM v5를 다운받았다. 1.MDK-ARM v5를 다운받기 위해 사이트에 들어갔다.(링크) 2.우측 상단의 Downloads버튼을 눌렀다. 3.Download Products에서 MDK-Arm을 선택한다. 4.아래와 같이 정보를 입력해주고 “Submit” 클릭! 5.그러면 .EXE파일을 다운받으라고 안내글이...","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(28)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-28)",
+        "excerpt":"디렉터리 구조에 대한 공부, 그리고 VMware 설치해보기 Local? LocalLow? Roaming? 오늘 ‘Keil uVision’을 지울려고 제어판에서 remove도 하고 경로추적해서 잡다한 데이터도 지우다가 해당 경로에 관심을 갖게 되었다. 바로 사용자 디렉토리 바로 아래에 있는 AppData라는 디렉토리이다. 그 아래에 다음과 같이 Local/LocalLow/Roaming이라는 디렉토리가 있었고, 내가 지우려는 STM32관련 데이터는 Local 아래 Arm이라는 폴더에 있었다....","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(29)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-29)",
+        "excerpt":"고전 신경망과 현대 신경망의 데이터 흐름에 대해서 발표준비 9.3.1.1 1차원 합성곱이라 하는데 뭔말인지 모르겠다. 예전에 신호및 시스템을 배웠을때 컨볼루션연산이라 함은 한 함수가 다른 함수 위에서 밀어서 이동하는 방식으로 진행하는 함수라 했다. 아래 예제코드는 1차원 신호에 대해서 컨볼루션 연산을 수행하는 예제라고 한다. x = np.linspace(-3, 3, 20) #넘파이 배열 생성 f...","categories": ["2024-01"],
+        "tags": ["python"],
+        "url": "/2024-01/codinglog(30)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-30)",
+        "excerpt":"미니콘다 설치해보기 Host PC에 미니콘다를 설치해보았다.(테스트용) 원래 1월25일자 코딩일지에서 동키카를 설치하는 험난한 여정을 거쳤다. 하지만 그럼에도 불구하고 제대로 되지 않았다. 그래서 이번에는 미니콘다를 등에 지고, 이게 있으면 버전관리가 잘 되기 때문에 엄청난 지원군이 된다. 그래서 이걸 뒤에 이고 깔아보겠다. cd C:\\User\\1\\workspace conda create -n donkey python=3.9 여기서 donkeycarworks는 프로젝트 이름이다....","categories": ["2024-01"],
+        "tags": ["C"],
+        "url": "/2024-01/codinglog(31)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-01-31)",
+        "excerpt":"오늘! 학습까지만이라도 시켜보자! 일단 PiRacer의 바퀴제어까지는 성공했다. 일단 교수님 도움으로 차량을 원격으로 구동시키는 것까지 끝냈다. 추가적으로 배터리의 상태라던가 이런것들 알아보고 싶어 chatGPT로 찾아봤더니 vcgencmd라는 모듈이 있다는 것을 알았다.(링크) 이 모듈로 다음 정보등을 알 수 있었다. 메모리 정보 pi@raspberrypi:~ $ vcgencmd get_mem arm &amp;&amp; vcgencmd get_mem gpu arm=948M gpu=76M 동작 온도...","categories": ["2024-01"],
+        "tags": ["python","shell"],
+        "url": "/2024-01/codinglog(32)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-02-01)",
+        "excerpt":"모터문제를 해결하고 학습을 진행하자! 모터문제를 해결하다. 어제 모터문제로 애를 먹었었다. 그래서 오늘 아침에 서보모터만 뜯어서 나의 아두이노에 연결을 해보았다. 아래는 그때 사용한 코드이다. #include &lt;Servo.h&gt; Servo servo; int value = 0; void setup() { servo.attach(7); Serial.begin(9600); } void loop() { if(Serial.available()) { char in_data; in_data = Serial.read(); if(in_data == '1')...","categories": ["2024-02"],
+        "tags": ["C","python"],
+        "url": "/2024-02/codinglog(33)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-02-03)",
+        "excerpt":"캐글에 가입해보았다!   캐글에 가입해보았다!   일단 사이트에 들어가주었다.      좌측상단의 “Sign in” 를 클릭하고,      나의 구글계정과 연동해서 “YUNSUNGWOONG”이라는 이름으로 가입을 해주었다.      ","categories": ["2024-02"],
+        "tags": ["python"],
+        "url": "/2024-02/codinglog(34)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-02-04)",
+        "excerpt":"dcworks..너란 녀석.. 어렵다.. dcworks에서 기본코드 돌려보기 내가 만약 아래 dcworks 디렉토리에서 아래로 들어가 library/donkeycar라는 내부 디렉토리에 들어가게 되면은 아래와 같이 donkey라는 패키지 관리자 명령어를 사용할 수 있다. donkey createcar --path ~/mycar 이때 ~/mycar를 하면 이 ~에 해당하는게 어딘지 추적해보니 아래와 같이 C:\\Users\\1이라고 한다. 그래서 나는 dcworks디렉토리 아래 자식디렉토리로 mycar를 만들기...","categories": ["2024-02"],
+        "tags": ["python"],
+        "url": "/2024-02/codinglog(35)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-02-08)",
+        "excerpt":"스로틀링 문제를 해결중이다. config.py코드 둘러보기 이전까지는 Pi-Racer에서 뒤로가기, 즉 후전을 하지 못했었는데, 성준이 형이 드디어 해결했다. 위와 같이 THROTTLE_REVERSE_PWM속성에 속성값을 220(기본값)에서 10~50사이로 조정해봤더니, 후전이 구현되는 것을 확인하였다. 원래 이걸로 학습데이터를 수집하고 학습결과로 “pilot.h5”모델을 export해야 하는데, 아직 그렇게 못했다. 그래서 일단은 트레이닝을 어떻게 시키는지에 대해 알아보는 중이다. 그래서 지금 launch.json파일을 보고...","categories": ["2024-02"],
+        "tags": ["python"],
+        "url": "/2024-02/codinglog(36)/",
+        "teaser": null
+      },{
+        "title": "코딩일지(2024-02-22)",
+        "excerpt":"오늘 드디어 파이레이서에 학습을 시켰다!! 파이레이서의 학습에 대해서… 오늘 드디어 파이레이서에 학습을 시켰다. 라즈베리파이에 학습시키는 건 너무 오래 걸릴 것 같아서, 일단 나의 랩탑으로 옮겨왔다. 근데 기존 명령어가 안먹혀서 일단은 dcworks에 있는 학습버튼을 눌러서 학습을 시켰다. 나중에는 donkey train -tub ./data -model ./models/mypilot.h5을 써먹어봐야 겠다. 혹시 될지도 모르겠다. 왜냐하면 2월...","categories": ["2024-02"],
+        "tags": ["shell"],
+        "url": "/2024-02/codinglog(37)/",
+        "teaser": null
+      }]
